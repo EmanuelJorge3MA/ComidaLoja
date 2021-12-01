99a91c7a66c870db5da8546145c034a9fd375643
emanueljorge <emanuel.jorge.130@gmail.com>
2021-12-01 09:48:35 +0100

    Added a file

---
+++ ComidaLoja/Helpers/AddFoodItemData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ComidaLoja.Model;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Xamarin.Forms;
+
+namespace ComidaLoja.Helpers
+{
+    public class AddFoodItemData
+    {
+        FirebaseClient client;
+        public List<FoodItem> FoodItems { get; set; }
+
+        public AddFoodItemData()
+        {
+            client = new FirebaseClient("https://comidaloja-default-rtdb.firebaseio.com/");
+            FoodItems = new List<FoodItem>()
+            {
+                new FoodItem
+                {
+                    ProductID = 1,
+                    CategoryID = 1,
+                    ImageUrl = "MainBurger",
+                    Name = "Burger and Pizza Hub 1",
+                    Description = "Burger - Pizza - Breakfast",
+                    Rating = "4.8",
+                    RatingDetail = " (121 raitings)",
+                    HomeSelected = "CompleteHeart",
+                    Price = 45
+                },
+                new FoodItem
+                {
+                    ProductID = 8,
+                    CategoryID = 3,
+                    ImageUrl = "MainDessert",
+                    Name = "Cakes",
+                    Description = "Cool Cakes- Breakfast",
+                    Rating = "4.8",
+                    RatingDetail = " (156 raitings)",
+                    HomeSelected = "EmptyHeart",
+                    Price = 45
+                }
+            };
+        }
+
+        public async Task AddFoodItemAsync()
+        {
+            try
+            {
+                foreach (var item in FoodItems)
+                {
+                    await client.Child("FoodItems").PostAsync(new FoodItem()
+                    {
+                        CategoryID = item.CategoryID,
+                        ProductID = item.ProductID,
+                        Description = item.Description,
+                        HomeSelected = item.HomeSelected,
+                        ImageUrl = item.ImageUrl,
+                        Name = item.Name,
+                        Price = item.Price,
+                        Rating = item.Rating,
+                        RatingDetail = item.RatingDetail
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ ComidaLoja/Model/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ComidaLoja.Model
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
